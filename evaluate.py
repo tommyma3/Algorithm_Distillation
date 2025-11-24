8c7227054479736a483fcdbc509c0725b6acc729
CutiRomero02 <124090467@link.cuhk.edu.cn>
2025-11-24 15:27:33 +0800

    update evaluate

--- evaluate.py
+++ evaluate.py
@@ -68,6 +68,9 @@ if __name__ == '__main__':
     with open(path, 'wb') as f:
         np.save(f, test_rewards)
 
+    for i in range(len(test_env_args)):
+        print(f'Env {i} (goal={test_env_args[i]}): {test_rewards[i]}')
+
     print("Mean reward per environment:", test_rewards.mean(axis=1))
     print("Overall mean reward: ", test_rewards.mean())
     print("Std deviation: ", test_rewards.std())
